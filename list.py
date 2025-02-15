ca376d465a893f410e889d479bb538dd6cee61d6
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-15 21:52:19 +0545

    list

---
+++ list.py
@@ -0,0 +1,27 @@
+# a list store multiple items in a single variable 
+
+member = ['manmohan', 'hari', 'ram', 'shyam', 'ravi' , 3, True] 
+
+#print(member)
+#print(member[1])
+#print(member[1] [0]) # it gives index 1 = Hari and its index 0 = H
+
+#print(member[1:]) # start from index 1 
+#print(member[1:4]) # start from index 1 to 4 
+
+
+#print(type(member))
+print(len(member))
+
+
+member[0] = "Aeir"  # change first index by Aeir 
+print(member)
+
+
+lists = list(('nepal', 'india', 'bhutn'))
+print(type(lists)) 
+
+ 
+
+
+ 
