481bbaa7a37ea75cbc3379bd67fb33ec7e7f3e5d
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-15 22:06:41 +0545

    list_methods

---
+++ list_methods.py
@@ -0,0 +1,24 @@
+l1 = [1,2,32,4,45,66]
+l2 = ["manmoan", "hari", "sita", "gita","hari"]
+
+#l1.extend(l2) # Combine in 1 
+#l2.append('added') # append in last 
+
+#l2.insert(1,'appple')   #add in specific position  index =1 
+#l2.remove('hari') #Remove   
+#l2.clear()
+
+#get incex of specific item 
+#print(l2.index('gita'))
+
+#print("Count of Hari is : ", l2.count('hari'))
+
+#l1.sort()
+#l1.reverse()
+#l2.pop() # remove last item 
+
+#l2.pop(1)  # remove by index 
+#del l2[0]
+del l2
+
+print(l2)
