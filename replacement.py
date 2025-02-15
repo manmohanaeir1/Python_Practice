352ec339b32d65b1623803b8030209d27dd0577d
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-15 21:38:23 +0545

    str replacement

---
+++ replacement.py
@@ -0,0 +1,7 @@
+sentence =  input("Enter sentence: ") # i am a student  
+print("Your Sentence is: ", sentence)
+
+w1 = input("Enter the word to replacement : ") # student 
+w2 = input("Enter the word to replace it with: ") # programmer 
+print(sentence.replace(w1,w2)) # i am a programmer
+
