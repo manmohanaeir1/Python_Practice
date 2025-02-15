cc37bc4c68ad305c10c4cba1aa689cd1d049dda6
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-15 21:30:16 +0545

    numbers

---
+++ number.py
@@ -0,0 +1,20 @@
+number = 40
+print(number + 34.5)
+
+
+n1 = str(number)
+# print(n1 + 4) gives error coz it is converted into string 
+
+print(n1) # this is string 
+
+
+print(max(45,3,55))
+print(abs(-3))
+
+print(round(3.5))
+
+
+from math import *
+print(bin(334))
+print(sqrt(64))
+
