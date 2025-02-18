d73d2c819bae1bbf0ddd325719a808535ee6e5a4
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-18 22:38:07 +0545

    tuple in python

---
+++ tuples.py
@@ -0,0 +1,14 @@
+# used to store multiple item in single variable 
+# it is immutable => we cannot alter items 
+# it alllows repetition of same values 
+
+
+
+
+number = (2,3,4,1,"hello")
+#print(number)
+print(number[1])
+print(len(number))
+print(type(number))
+
+
