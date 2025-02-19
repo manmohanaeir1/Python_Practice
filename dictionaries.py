313edbb17d26260c0087239eb7d02c62b73bdf48
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-19 22:01:03 +0545

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+# is used to store data in key value pairs 
+# changable and indexed
+# dont allow duplicate values
+
+
+dct = {
+    "name" : "Manmohan",
+    "name1" : "Manmohan",
+    "roll": 23,
+    "Address": "MNR", 
+
+    "firends" : ["Ram", "Shyam", "Hari"]
+
+}
+
+#print(dct)
+
+print(dct["firends"])
