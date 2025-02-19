606ac9522652ff21f08c61fd64b1ec6488af3f8d
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-19 22:16:29 +0545

    2d_list in python

---
+++ 2d_list.py
@@ -0,0 +1,14 @@
+#two dimension list
+
+numb = [
+       [1,2,3],
+       [4,5,6], 
+       [7,8,9]
+       ]
+#print(numb)
+
+print(numb[1][2])
+
+for lists in numb:
+   for list in lists:
+       print(list) 
