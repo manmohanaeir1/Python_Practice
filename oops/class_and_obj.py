9e17d7a47912b2f112f53cd47bef6064a8eec808
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-21 23:13:34 +0545

    Class and objects

---
+++ oops/class_and_obj.py
@@ -0,0 +1,17 @@
+#class is like a cunstructor in 
+
+"""class Student:
+    roll = 5
+
+s1 = Student()
+print(s1.roll)   """
+
+
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = person("John", 36)
+print(p1.name)  
+print(p1.age)      
