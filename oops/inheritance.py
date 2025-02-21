c93f04802a6958c60fe56590c92bd18ac1e27b03
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-02-21 23:19:34 +0545

    Inheritance in python

---
+++ oops/inheritance.py
@@ -0,0 +1,5 @@
+class students():
+
+    name = "Manmohan"
+    age = 21
+    gender ='male'
---
+++ oops/inheritance1.py
@@ -0,0 +1,7 @@
+from inheritance import students
+
+class Person(students):
+    pass
+
+p1 = Person()
+print(p1.name)
